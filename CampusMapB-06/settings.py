b882d09b53ac794ec1e35b371e375c791fa136c4
Ryan Gibiser <rmg5ft@virginia.edu>
2021-03-15 23:58:50 -0400

    change settings.py app name

--- CampusMapB-06/settings.py
+++ CampusMapB-06/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',   # <--
-    'CampusMapB-06',   # <--
+    'maps',   # <--
 
     'allauth',   # <--
     'allauth.account',   # <--
