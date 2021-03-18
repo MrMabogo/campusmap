6aa88d0c4c9eec6e337a5817ed4f3e69d62962b7
Brian <brianmsbsdcs@gmail.com>
2021-03-17 20:00:27 -0400

    added intial map

--- maps/views.py
+++ maps/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+def default_map(request):
+    mapbox_access_token = 'pk.my_mapbox_access_token'
+    return render(request, 'maps/default.html',
+                  { 'mapbox_access_token': mapbox_access_token })
