b9d7f27adf1e2d7f5b9e51b0f08b04607ef5ba15
Brian <brianmsbsdcs@gmail.com>
2021-03-20 17:14:45 -0400

    default map and empty tests

--- maps/tests.py
+++ maps/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
 
-# Create your tests here.
+class NavigationTests(TestCase):
+    def test_search_for_fake_location(self):
+        '''
+        Expected arrival to location that doesn't exist
+        '''
+        self.assertIs(True, True) #placeholder
+    def test_eta_to_here(self):
+        '''
+        Expected arrival to current location
+        '''
+        self.assertIs(True, True) #placeholder
+    def test_multiple_etas(self):
+        '''
+        Expected arrival to multiple different locations
+        '''
+        self.assertIs(True, True) #placeholder
+    def test_save_empty_route(self):
+        '''
+        Save route without specifying
+        '''
+        self.assertIs(True, True) #placeholder
+    def test_save_multiple_routes(self):
+        '''
+        Save multiple valid routes
+        '''
+        self.assertIs(True, True) #placeholder
--- maps/views.py
+++ maps/views.py
@@ -4,3 +4,19 @@ def default_map(request):
     mapbox_access_token = 'pk.my_mapbox_access_token'
     return render(request, 'maps/default.html',
                   { 'mapbox_access_token': mapbox_access_token })
+
+def map_search(request):
+    pass
+
+def get_eta(request):
+    pass
+
+def save_route(request):
+    pass
+
+def get_routes(request):
+    pass
+
+def display_routes(request):
+    pass
+    
