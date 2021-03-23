5806deebcd9fe6f0e39b642fb81bb8f9698aaf6e
Ryan Gibiser <rmg5ft@virginia.edu>
2021-03-22 20:14:27 -0400

    fix urls setup

--- maps/urls.py
+++ maps/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 app_name = 'maps'
 urlpatterns = [
-    path('', views.index_view, name='index'),
+    # path('', views.index_view, name='index'),
     path('', views.default_map, name="default"),
 ]
