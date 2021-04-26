c096983ee13a65806e6dc29488d67c08e8cb22e6
Brian <brianmsbsdcs@gmail.com>
2021-04-25 20:06:30 -0400

    save page and acess utilities

--- maps/admin.py
+++ maps/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import SavedRoute
-from .models import UVALocation
+from .models import SavedRoute, UVALocation, UVALocationCollection
 
 # Register your models here.
 admin.site.register(SavedRoute)
 admin.site.register(UVALocation)
+admin.site.register(UVALocationCollection)
